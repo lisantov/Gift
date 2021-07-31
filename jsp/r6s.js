7bc102d3ec38f9ea01c63ba442d46e2cf42f16d5
lisantov <71330868+lisantov@users.noreply.github.com>
2021-07-31 19:30:14 +0700

    v0.1 - Unfinished project take it if you want
    v0.1 - Unfinished project take it if you want

---
+++ jsp/r6s.js
@@ -0,0 +1,38 @@
+let sliderImageIndex = 0;
+
+function nextSlide()
+{
+	if (sliderImageIndex < 3)
+	{
+		sliderImageIndex+=1;
+		moveSlide();
+	}
+}
+function previousSlide()
+{
+	if (sliderImageIndex >= 1)
+	{
+		sliderImageIndex-=1;
+		moveSlide();
+	}
+}
+
+function moveSlide()
+{
+	let slides = document.getElementsByClassName("slide");
+	let slideWidth = slides[sliderImageIndex].width;
+
+	let slideMargin = slideWidth * sliderImageIndex;
+	let slidesList = document.getElementById("slidesList");
+ 	slidesList.style.left = -slideMargin + 'px';
+}
+
+function init()
+{
+    let buttonNext =  document.getElementById("buttonNext");
+    nextSlide();
+    buttonNext.addEventListener ("click", nextSlide);
+    let buttonPrevious =  document.getElementById("buttonPrevious");
+    buttonPrevious.addEventListener ("click", previousSlide);
+}
+setTimeout(init, 0);
